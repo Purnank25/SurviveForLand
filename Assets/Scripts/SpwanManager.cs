6a1747763efc40956c21dab558e51241e134c50a
Purnank25 <purnankexcel1@gmail.com>
2025-06-02 16:57:41 +0530

    Added random spwaned enemies

--- Assets/Scripts/SpwanManager.cs
+++ Assets/Scripts/SpwanManager.cs
@@ -3,16 +3,26 @@ using UnityEngine;
 public class SpwanManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public float spwanRange = 9.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 0),enemyPrefab.transform.rotation);
-        
+      
+        Instantiate(enemyPrefab, GenerateSpwanPos(), enemyPrefab.transform.rotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private Vector3 GenerateSpwanPos()
+    {
+        float spwanPosX = Random.Range(-spwanRange, spwanRange);
+        float spwanPosZ = Random.Range(-spwanRange, spwanRange);
+        Vector3 randomPos = new Vector3(spwanPosX, 0, spwanPosZ);
+        return randomPos;
     }
 }
